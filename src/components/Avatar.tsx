d3970b99e049420a9d715a461b6bcb38da636b27
ThaSMorato <tha.s.morato@gmail.com>
2023-11-18 16:16:34 -0300

    spread props

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,18 +1,18 @@
 
+import { ImgHTMLAttributes } from 'react';
 import styles from './Avatar.module.css';
 
-interface AvatarProps {
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement>{
   hasBorder?: boolean;
-  src: string;
-  alt?: string;
 }
 
-function Avatar({ src, hasBorder = true, alt}: AvatarProps) {
+function Avatar({ src, hasBorder = true, alt, ...props}: AvatarProps) {
   return (
     <img
       className={hasBorder ? styles.avatarWithBorder : styles.avatar}
       src={src}
       alt={alt}
+      {...props}
     />
   );
 }
